7cf997b8d986de6b46e3c091c4bc9de4785399fa
Adinda Riztia <adindariztia@gmail.com>
2018-11-25 15:20:21 +0700

    added login function, connected to database

---
+++ main.js
@@ -0,0 +1,39 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function login(){
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:5000/login',
+        beforeSend: function(req) {
+            req.setRequestHeader("Content-Type", "application/json");
+        },
+        data : JSON.stringify({
+            "email": document.getElementById('email').value,
+            "password": document.getElementById('password').value
+    
+        }),
+        success: function(res){
+            document.cookie = `token=${res}`
+            window.location = "/dashBoard.html"
+        },
+        error: function(err) {
+            console.log(err)
+
+
+        }
+    })
+}
